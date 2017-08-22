e6b707d43a3564fc7d842a5967356d9db232cbc3
OhunterO <shciairong@126.com>
2017-08-22 10:13:44 +0800

    firstcommit

---
+++ src/main/java/com/joyone/test/JoyoneApplication.java
@@ -0,0 +1,12 @@
+package com.joyone.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyoneApplication.class, args);
+	}
+}
