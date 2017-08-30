cbf1fde1d0853a4b04aba660ff8e12beb89a5a01
OhunterO <shicairong@126.com>
2017-08-30 21:50:00 +0800

    page commit

--- src/main/java/com/joyone/test/controller/TestController.java
+++ src/main/java/com/joyone/test/controller/TestController.java
@@ -1,16 +1,25 @@
 package com.joyone.test.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
+@Controller
 @RequestMapping(value="/test")
 public class TestController {
 
     @RequestMapping(method = GET)
+    @ResponseBody
     public String test(){
         return "test success";
     }
+
+    @RequestMapping(value="index",method = GET)
+    public String testPage(){
+        return "test/index";
+    }
+
 }
