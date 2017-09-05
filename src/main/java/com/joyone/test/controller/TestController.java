4c2bef86c53e1695931dc3895beae849bef04120
OhunterO <shicairong@126.com>
2017-09-05 22:34:33 +0800

    update

--- src/main/java/com/joyone/test/controller/TestController.java
+++ src/main/java/com/joyone/test/controller/TestController.java
@@ -5,15 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping(value="/test")
 public class TestController {
-
+    private String testStr=null;
     @RequestMapping(method = GET)
     @ResponseBody
-    public String test(){
+    public String test(HttpServletRequest request){
+        String code = request.getParameter("code");
+        if(code!=null){
+            testStr = code;
+        }
+        if(testStr!=null){
+            return testStr;
+        }
         return "test success";
     }
 
