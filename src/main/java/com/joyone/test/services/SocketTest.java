1a77b05fc4a24b25527b034164c4c8e177c18f6a
shicairong@126.com <shi62247307>
2017-09-14 11:33:01 +0800

    update photo

--- src/main/java/com/joyone/test/services/SocketTest.java
+++ src/main/java/com/joyone/test/services/SocketTest.java
@@ -4,7 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 @Component
 @Order(value=1)
@@ -12,6 +16,29 @@ public class SocketTest implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         System.out.println("Scoket Test.................");
-        ServerSocket ss= new ServerSocket(8567);
+        ServerSocket serversocket= new ServerSocket(8567);
+        while(true){
+            Socket s =serversocket.accept();
+            new AcceptThread(s).start();
+        }
+
+    }
+    class AcceptThread extends Thread{
+        private Socket s;
+        public AcceptThread(Socket s){
+            this.s=s;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()) );
+                pw.println("connect success");
+                pw.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
